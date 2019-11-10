056e7ce78fee4761754bfba0a0bbf3286c6509ec
Luminiscental <luminiscental01@gmail.com>
2019-11-10 01:49:38 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+# pre-build actions:
+
+clang-format -i -style=file ./src/* ./include/**/* # auto-format
+export CXX=/usr/bin/clang++ # g++ has appalling errors
+
 mkdir -p build
-
-clang-format -i -style=file ./src/* ./include/**/*
-
 pushd build
+
+# build:
 
 rm -f CMakeCache.txt
 cmake .. -DTESTS=ON
 make
+
+# test:
+
 ./mth_test
 
 popd
